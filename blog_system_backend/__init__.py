657b8858b9d0c8f43f950d3db288e14a157cd2ad
mysthz <mchokopayeva@mail.ru>
2025-12-17 16:45:00 +0300

    Сохраненные посты

--- blog_system_backend/__init__.py
+++ blog_system_backend/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["Post", "User", "Category", "PostToCategory", "Comment"]
+__all__ = ["Post", "User", "Category", "PostToCategory", "Comment", "SavedPost"]
 
 from blog_system_backend.src.api.categories.models import Category, PostToCategory
 from blog_system_backend.src.api.posts.comments.models import Comment
 from blog_system_backend.src.api.posts.models import Post
+from blog_system_backend.src.api.posts.saved_posts.models import SavedPost
 from blog_system_backend.src.api.users.models import User
